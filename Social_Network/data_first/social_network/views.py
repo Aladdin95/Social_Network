513a40df014fefe2c10c9a14672ce0e680674edc
Aladdin <alaa2005721@gmail.com>
2018-04-29 21:32:26 +0200

    using images and css files

--- Social_Network/data_first/social_network/views.py
+++ Social_Network/data_first/social_network/views.py
@@ -6,7 +6,7 @@ from .models import User
 def index(request):
     all_users = User.objects.all()
     context = {'all_users': all_users}
-    return render(request, 'social_network/index.html', context)
+    return render(request, 'social_network/home.html', context)
 
 
 def detail(request, user_id):
