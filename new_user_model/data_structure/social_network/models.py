d1c7cc045d544cd28e1a4a4206ab075dca1867d8
Aladdin <alaa2005721@gmail.com>
2018-05-04 09:23:01 +0200

    modifing models birth_date, gender and mobile_number

--- new_user_model/data_structure/social_network/models.py
+++ new_user_model/data_structure/social_network/models.py
@@ -9,9 +9,9 @@ class CustomUser(AbstractUser):
     email = models.CharField(max_length=100, unique=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    birth_date = models.CharField(max_length=100)
-    gender = models.CharField(max_length=100)
-    mobile_number = models.CharField(max_length=100)
+    birth_date = models.DateField(max_length=10)
+    gender = models.CharField(max_length=8)
+    mobile_number = models.CharField(max_length=15)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
